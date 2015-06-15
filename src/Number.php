85c2d62785022433eb49a22e7b80693b8fd48dd4
omaximus <maxomelch93@gmail.com>
2015-06-15 17:07:26 +0300

    numeric_value field is used now

--- src/Number.php
+++ src/Number.php
@@ -13,6 +13,9 @@ class Number extends Field
     /** @var string Special CSS classname for nested field objects to bind JS and CSS */
     public $cssClass = '__number';
 
+    /** Database object field name */
+    protected $param = 'numeric_value';
+
     /**
      * Function to convert field value before save
      *
